ae9eeabb59fb94cc80ca47929020141319bec4d0
Kyle <wong.kyle5@gmail.com>
2013-08-04 22:05:43 -0700

    Update get_name.py

--- python/get_name.py
+++ python/get_name.py
@@ -8,13 +8,6 @@ from bs4 import BeautifulSoup
 import re
 import urllib2
 
-#response = urllib2.urlopen("http://chem.sis.nlm.nih.gov/chemidplus/ProxyServlet?objectHandle=DBMaint&actionHandle=default&nextPage=jsp/chemidheavy/ResultScreen.jsp&ROW_NUM=0&TXTSUPERLISTID=0022771444")
-#response = urllib2.urlopen("http://chem.sis.nlm.nih.gov/chemidplus/")
-#response = urllib2.urlopen("http://chem.sis.nlm.nih.gov/chemidplus/ProxyServlet?objectHandle=DBMaint&actionHandle=default&nextPage=jsp/chemidheavy/ResultScreen.jsp&ROW_NUM=0&TXTSUPERLISTID=0029414560")
-#response = urllib2.urlopen("http://pubchem.ncbi.nlm.nih.gov/")
-#response = urllib2.urlopen("https://www.google.com/")
-#response = urllib2.urlopen("http://www.packtpub.com/article/web-scraping-with-python-part-2")
-
 #response = urllib2.urlopen("http://pubchem.ncbi.nlm.nih.gov/summary/summary.cgi?cid=7476")
 for site in open("list"):
 	response = urllib2.urlopen(site)
@@ -25,18 +18,3 @@ for site in open("list"):
 	left = page_source.find("<title>")
 	right = page_source.find("</title>")
 	print page_source[left+7:right-10]
-#if "<title>" in page_source:
-#	print line
-
-#soup = BeautifulSoup(page_source)
-
-#contact = {}
-#for line in soup.findAll("table"):
-#	print line
-#	s = line.find('span')
-#	if s:
-#	print s
-
-#t = soup.findAll('')
-#print t
-
