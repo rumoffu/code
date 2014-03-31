154794177aa333586a88c70914d8a3a22cb3fb78
Kyle <wong.kyle5@gmail.com>
2014-03-31 12:37:44 -0400

    testing with selenium

--- python/weburl.py
+++ python/weburl.py
@@ -14,23 +14,38 @@ import urllib2
 
 
 def main():
+  #driver = webdriver.Chrome()
+  #driver = webdriver.Chrome(executable_path="/home/sirus/opt/chromedriver")
   driver = webdriver.Firefox()
   #link = "http://www.sigmaaldrich.com/catalog/product/aldrich/137499?lang=en&region=US"
   link = "http://www.sigmaaldrich.com/catalog/ProductDetail.do?N5=SEARCH_CONCAT_PNO&N4=245542|ALDRICH"
   
   #print content
-  printLink(link)
-  return
+  #printLink(link)
+
   # Get Redirected link content
   driver.get(link)
   #Wait for it to load
+  count = 0
   while(link == driver.current_url):
     time.sleep(1)
+    print 'loading %s' % count
+    count =+ 1
   redir_url = driver.current_url
   
   #print content
-  printLink(link)
+  #printLink(link)
 
+  driver.quit()
+  #driver.close()
+  #print 'close'
+  
+def executeJS(link):
+  driver = webdriver.Firefox()
+  driver.get(link)
+  resp = driver.execute_script("return 5")
+  print resp
+  driver.quit()
   
 def printLink(link):
   response = urllib2.urlopen(link)
