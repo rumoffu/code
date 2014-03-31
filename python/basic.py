f486b694a47f7436df8450d78475ec28a85c4046
Kyle <wong.kyle5@gmail.com>
2014-03-31 11:04:23 -0400

    adding basic starter file

---
+++ python/basic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Description
+'''
+Example input:
+7476	4-acetylanisole
+7478	4-anisic acid
+7501	Styrene
+
+Example output:
+
+'''
+
+def main():
+  
+
+if __name__ == '__main__':
+  main()
+
+
